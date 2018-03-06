f780c6b235f0e8fe288dc8749c01f7f904e6ac1e
nycsamuel <xsangmin@gmail.com>
2018-03-06 11:13:46 -0500

    Add public folder; Add view engine -ejs

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const app        = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
