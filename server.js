bef4b9e2d58940447625eb4915134d2e6a7796fe
nycsamuel <xsangmin@gmail.com>
2018-03-06 11:08:58 -0500

    Add server file

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict'
+
+require('dotenv').config();
+const express    = require('express');
+const logger     = require('morgan');
+const path       = require('path');
+const bodyParser = require('body-parser');
+const PORT       = process.argv[2] || process.env.PORT || 3000;
+const app        = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+
+app.listen(PORT, () => console.log('LISTENING TO PORT: ', PORT));
